3341448700bdcf806c864cae820c766b4569f1f5
WILLIAMS Jason <jason.williams2@soprasteria.com>
2023-09-04 14:03:07 +0200

    removed unused variable declaration

--- src/main/java/org/example/LeapYear.java
+++ src/main/java/org/example/LeapYear.java
@@ -3,7 +3,6 @@ package org.example;
 public class LeapYear {
 
     public boolean isLeapYear(int year){
-        boolean result;
         if (year == 2023) return false;
         return true;
     }
